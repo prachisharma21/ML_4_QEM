a2d00f83719c010da582f8c92f44b77c27e88a66
prachisharma21 <prachi16490@gmail.com>
2024-05-07 16:23:22 +0200

    added comments

--- random_VQE_data_prep.py
+++ random_VQE_data_prep.py
@@ -8,7 +8,6 @@ import pickle
 class CircuitBuilder():
     def __init__(self,params,backend , initial_layout , geometry, nlayers =int):
         self.backend = backend
-        
         self.initial_layout = initial_layout
         self.geometry = geometry        
         self.params = params
@@ -16,18 +15,18 @@ class CircuitBuilder():
         self.nlayers = nlayers
        
 
-    def vqeLayer_FakeQuito(self,params):#theta_ZZ, theta_Z, theta_X): #params has to be a list a list for each layer
+    def vqeLayer_FakeQuito(self,params): #params has to be a list a list for each layer
         """ VQE layer for the FakeQuito geometry using all qubits and native connectivity"""
         theta_Z = params[0] # make bonds a list of bond list 
         theta_X = params[1]
         theta_ZZ = params[2]
         vqeLayer = QuantumCircuit(self.num_qubits)
         # Choosen bond pairs according to the native qubit connectivity of the backend
-        bonds_1 = [[0, 1], [3, 4]]  # these bond pairs should be accessible as well-- they will be needed for hamiltonian expectation for eg. 
-        bonds_2 = [[1, 2]] # could be added to the main QuantumSystem class as they are dependent on the backend geometry
+        bonds_1 = [[0, 1], [3, 4]]  
+        bonds_2 = [[1, 2]]
         bonds_3 = [[1, 3]]
         # the RZ and RZ terms for the field terms of the hamiltonian. 
-        #Applied first to get the sequence of layers for PER later to come out correctly, i.e., single qubit gates first followed by clifford gates. 
+        # Applied first to get the sequence of layers for PER later to come out correctly, i.e., single qubit gates first followed by clifford gates. 
         
         vqeLayer.rz(theta_Z, range(self.num_qubits))
         vqeLayer.rx(theta_X, range(self.num_qubits))
@@ -43,47 +42,9 @@ class CircuitBuilder():
         vqeLayer.cx(*zip(*[bonds_3[i] for i in range(len(bonds_3))]))
         vqeLayer.rz(theta_ZZ, [bonds_3[i][1] for i in range(len(bonds_3))])
         vqeLayer.cx(*zip(*[bonds_3[i] for i in range(len(bonds_3))]))
-   
-        #vqeLayer.barrier()
 
         return vqeLayer
     
-    
-    # Having intial_layout is better---as then you can choose not to use all qubits. 
-    def makevqeCircuit(self, measure = False, meas_basis = "Z"): # NEED to figure out how to input measure and basis here 
-        
-        vqeCircuit = QuantumCircuit(self.num_qubits)
-        for i in range(int(len(self.params)/3)): #len params list /number of parameters in the circuit => 6/3-1 = 1 and range(1)=>[0,1]
-            #print("i = ",i)
-            if self.geometry == "FakeCasablancaV2":
-                vqeCircuit.h(range(self.num_qubits)) # initialize in the |+> state
-                vqeCircuit.barrier()
-                vqeL = self.vqeLayer_Casablanca(self.params[3*i:3*(i+1)]) # self.theta_ZZ_L_1[i], self.theta_Z_L_1[i], self.theta_X_L_1[i])
-            elif self.geometry == "FakeQuitoV2":
-                #print("here")
-                vqeCircuit.h(range(self.num_qubits)) # initialize in the |+> state
-                vqeCircuit.barrier()
-                vqeL = self.vqeLayer_FakeQuito(self.params[3*i:3*(i+1)]) #self.theta_ZZ_L_1[i], self.theta_Z_L_1[i], self.theta_X_L_1[i])
-            elif self.geometry == "FakeGuadalupeV2":
-                vqeCircuit.h(range(self.num_qubits)) # initialize in the |+> state
-                vqeCircuit.barrier()
-                vqeL = self.vqeLayer_FakeGuadalupeV2(self.params[3*i:3*(i+1)])#self.theta_ZZ_L_1[i], self.theta_Z_L_1[i], self.theta_X_L_1[i])
-            vqeCircuit = vqeCircuit.compose(vqeL)
-            vqeCircuit.barrier()
-               
-        if measure == True:
-            if meas_basis == "Z":
-                vqeCircuit.measure_all()
-                transpiled = transpile(vqeCircuit, self.backend, initial_layout = self.initial_layout)
-            elif meas_basis =="X":
-                vqeCircuit.h(range(self.num_qubits))
-                vqeCircuit.measure_all()
-                transpiled = transpile(vqeCircuit, self.backend, initial_layout = self.initial_layout)
-            else: 
-                print("Measurement not defined")    # Y-measurements can be added
-        else: 
-            transpiled = vqeCircuit # transpile(vqeCircuit, self.backend, initial_layout = self.initial_layout)
-        return transpiled
     
 # Having intial_layout is better---as then you can choose not to use all qubits. 
     def makevqeCircuit_(self, measure = False, meas_basis = "Z"): # NEED to figure out how to input measure and basis here 
@@ -91,15 +52,13 @@ class CircuitBuilder():
         vqeCircuit = QuantumCircuit(self.num_qubits)
         vqeCircuit.h(range(self.num_qubits)) # initialize in the |+> state
         vqeCircuit.barrier()
-        for i in range(self.nlayers): #len params list /number of parameters in the circuit => 6/3-1 = 1 and range(1)=>[0,1]
+        for i in range(self.nlayers): 
             if self.geometry == "FakeQuitoV2":
-                #print("here")
-                vqeL = self.vqeLayer_FakeQuito(self.params[3*i:3*(i+1)]) #self.theta_ZZ_L_1[i], self.theta_Z_L_1[i], self.theta_X_L_1[i])
+                vqeL = self.vqeLayer_FakeQuito(self.params[3*i:3*(i+1)]) 
             elif self.geometry == "FakeGuadalupeV2":
-                vqeL = self.vqeLayer_FakeGuadalupeV2(self.params[3*i:3*(i+1)])#self.theta_ZZ_L_1[i], self.theta_Z_L_1[i], self.theta_X_L_1[i])
+                vqeL = self.vqeLayer_FakeGuadalupeV2(self.params[3*i:3*(i+1)])
             vqeCircuit = vqeCircuit.compose(vqeL)
             vqeCircuit.barrier()
-        #print(vqeCircuit)
                
         if measure == True:
             if meas_basis == "Z":
@@ -127,7 +86,7 @@ class CircuitBuilder():
             circ_w_no_meas.measure_all()
             transpiled = transpile(circ_w_no_meas, self.backend, initial_layout = self.initial_layout)
         elif meas_basis =="X":
-            circ_w_no_meas.h(meas_qubit)#range(self.num_qubits))
+            circ_w_no_meas.h(meas_qubit)
             circ_w_no_meas.measure_all()
             transpiled = transpile(circ_w_no_meas, self.backend, initial_layout = self.initial_layout)
         elif meas_basis =='Y':
@@ -137,7 +96,7 @@ class CircuitBuilder():
             transpiled = transpile(circ_w_no_meas, self.backend, initial_layout = self.initial_layout)
 
         else: 
-                print("Measurement not defined")    # Y-measurements can be added
+                print("Measurement not defined")  
 
         return transpiled
 
@@ -149,10 +108,7 @@ def State_Vector_Simulator(circuits):
     return count_SV
 
 def QASM_Simulator(circuits,shots=1000):
-
-    # For Z measurements
-    count_QASM =Aer.get_backend('qasm_simulator').run(circuits[0], shots=shots).result().get_counts()
-        
+    count_QASM =Aer.get_backend('qasm_simulator').run(circuits[0], shots=shots).result().get_counts()    
     return count_QASM
 
 def Noisy_backend_Simulato(circuits,backend = FakeQuitoV2(),shots=1000):
@@ -215,20 +171,20 @@ def Vcircuit_w_random_params(backend =FakeQuitoV2(), n_layers = 1, num_params = 
                              , geometry="FakeQuitoV2",nlayers =n_layers)
     # Create circuit with no measurements for the state-vector calculations to find the ideal expectation value of the observable
     circ_w_no_meas = [Vcircuit.makevqeCircuit_(measure = False)]
-    print(circ_w_no_meas[0].draw())
+    ### print(circ_w_no_meas[0].draw())
     # Create circuits with specific measurements
     circ_w_meas = [Vcircuit.circ_w_measurements(meas_basis ,meas_qubit)]
-    print(circ_w_meas[0].draw())
+    ### print(circ_w_meas[0].draw())
 
     return circ_w_no_meas, circ_w_meas
-#Vcircuit_w_random()
+
 
 def data_preparation():
     # we are choosing Quito fake backend 
     Qbackend = FakeQuitoV2()
     # number of layers can be choosen randomly from 1 to 5
     num_layers = np.random.choice([i+1 for i in range(5)])
-    print(num_layers)
+    ### print(num_layers)
     # We have only 3 parameters in each layer because of the Mixed field Ising model chosen here. Therefore, it is hard coded
     num_params = 3
     # shots of measurements 
@@ -236,7 +192,7 @@ def data_preparation():
 
     # pick a random observable to measure from a list of all single-qubit measurements
     observable= pick_random_observable()
-    print("observable= pick_random_observable()", observable)
+    ### print("observable= pick_random_observable()", observable)
     # Find the measurement gates and qubit to apply those on. 
     # the following function can be updated later for commuting measurements: For the time it is a simplest implementation
     meas_qubit, meas_gate = find_measurement_basis(observable= observable)
@@ -249,17 +205,17 @@ def data_preparation():
     SV_count = State_Vector_Simulator(circuits=circ_w_no_meas)
     obs = Pauli(observable)
     ideal_expectation = SV_count.expectation_value(obs)
-    print(ideal_expectation)
+    ### print(ideal_expectation)
     # performing the Quantum simulations to find the noisy expectation value of the observables
     Noisy_count = Noisy_backend_Simulato(circuits=circ_w_meas, backend=Qbackend , shots = num_shots)
     noisy_expectation = expectation_value_single_qubit(count = Noisy_count,active_qubit = meas_qubit)
-    print(noisy_expectation)
+    ### print(noisy_expectation)
     return [circ_w_meas, num_layers, observable, ideal_expectation, noisy_expectation]
 
 
 from collections import defaultdict
 
-def data_storage(num_circuits_for_ML = 1):
+def data_storage(num_circuits_for_ML = 1000):
     # TASK-1
     # run the loop below to create set of 1000 circuits and then spilt that into training and test dataset
     # you could choose if you want to have an evaluation dataset
@@ -288,7 +244,7 @@ def main():
     # from this loaded dictionary object with key Quantum_circuit, one can extract the all the other features
     # the rest of the keys are also features for the model
     # TASK 2  
-    # you can write a small fucntion to load each pickle data and extract the extra features and create a dataframe with all features included.  
+    # you can write a small function to load each pickle data and extract the extra features and create a dataframe with all features included.  
 
 
 
