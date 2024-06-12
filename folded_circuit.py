687c37349439dc5471c4c51d07a8bb9102f4c936
prachisharma21 <prachi16490@gmail.com>
2024-06-12 13:07:56 +0200

    updated_folded circuit for the data prep

--- folded_circuit.py
+++ folded_circuit.py
@@ -22,3 +22,25 @@ def fold_circuit(file_path):
     print(f"Noisy expectation value with scaled circuit: {noisy_expectation}")
 
     return dict({'scaled_circuit': scaled_circuit, 'noisy_count': noisy_count, 'noisy_expectation': noisy_expectation})
+
+def data_storage_Folded_circuit(file_name = 'circ_0.pickle'):
+ data_dict = defaultdict() # picking a random noise scaling factor that is above 1 and less than 2 
+ noise_scale_factor = round(np.random.uniform(1.01, 1.8, 1)[0],2) 
+ single_circ = data_loader(file_name) 
+ scaled_circuit = zne.scaling.fold_gates_at_random(single_circ["Quantum_circuit"][0], scale_factor=noise_scale_factor) #fidelities={"single": 1.0, "CNOT": 0.99} ) 
+ obs = single_circ["obervable"] 
+ meas_qubit, meas_gate = find_measurement_basis(observable= obs) 
+ Noisy_count =Noisy_backend_Simulator(circuits= [scaled_circuit], backend=FakeQuitoV2() , shots = 100000) 
+ noisy_expectation = expectation_value_single_qubit(count = Noisy_count,active_qubit = meas_qubit) 
+ data_dict["Quantum_circuit"] = scaled_circuit 
+ data_dict["num_layers"] = single_circ["num_layers"] 
+ data_dict["obervable"] = single_circ["obervable"] 
+ data_dict["ideal_expectation"] = obs 
+ data_dict["noisy_expectation"] = noisy_expectation # extra column which could also be added separately to all the rest of the existing circuits 
+ data_dict["scale_factor"] = noise_scale_factor 
+ with open(file_name.split(".")[0]+"_scaled.pickle", "wb") as f: 
+    pickle.dump(data_dict,f)
+
+
+    
+data_storage_Folded_circuit()
